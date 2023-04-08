879a11ae17191699d4e384192ccabad519b14ff2
brianrahma <brianrahma549@gmail.com>
2023-04-08 22:31:25 +0700

    first

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,9 @@
 !(function ($) {
   "use strict";
-
   $(document).on("click", ".mobile-nav-toggle", function (e) {
     $("body").toggleClass("mobile-nav-active");
-    $(".mobile-nav-toggle i").toggleClass("icofont-navigation-menu icofont-close"
-    );
+    $(".mobile-nav-toggle i").toggleClass("icofont-navigation-menu icofont-close");
+    $(".toggle-btn").hide();
   });
 
   $(document).click(function (e) {
